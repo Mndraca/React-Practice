84d3d69aac5330f051fca4f5a354bea65229c7d2
Mndraca <marizanovic3@gmail.com>
2024-06-17 18:04:39 -0400

    my first commit

---
+++ src/components/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//function Greet (){
+ //   return<h1>Hello React! Im here to stay!<</h1>
+//}
+export const Greet = () => <h1>Hello React!</h1>
+
+//export default Greet
